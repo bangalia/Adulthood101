b9ad6ccf3031af1ac5f8aa146153d31ff23a19a7
lotsofluck <anitabangali@yahoo.com>
2021-03-21 12:00:56 -0500

    app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """A homepage with handy links for your convenience."""
+    return render_template('home.html') 
